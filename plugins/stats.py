729f8916caac9c4a1e293c306c70d7ddc1d1a95a
Sihad <sihadmiah19@gmail.com>
2024-02-12 20:08:33 +0600

    x

---
+++ plugins/stats.py
@@ -0,0 +1,44 @@
+import time
+import shutil
+from pyrogram import Client, filters, enums
+
+
+BOT_START_TIME = time.time()
+@Client.on_message(filters.command('status') & filters.user(555994473))
+async def bot_status(client,message):
+    uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - BOT_START_TIME))
+    try:
+        t, u, f = shutil.disk_usage(".")
+        total = humanbytes(t)
+        used = humanbytes(u)
+        free = humanbytes(f)
+
+        disk = "\n**Disk Details**\n" \
+            f"> USED  :  {used} / {total}\n" \
+            f"> FREE  :  {free}\n\n"
+    except:
+        disk = ""
+
+    await message.reply_text(
+        "𝗖𝘂𝗿𝗿𝗲𝗻𝘁 𝘀𝘁𝗮𝘁𝘂𝘀 𝗼𝗳 𝘆𝗼𝘂𝗿 𝗕𝗼𝘁\n\n"
+        "DB Status\n"
+        f"➪ 𝖡𝗈𝗍 𝖴𝗉𝗍𝗂𝗆𝖾: {uptime}\n"
+        f"{disk}",
+        quote=True,
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
+
+ # humanbytes
+    
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: '', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
